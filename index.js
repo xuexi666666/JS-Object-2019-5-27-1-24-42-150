f669da822e7cd2524ee861bcc6f33ed921953069
Jackyun <519775137@qq.com>
2019-07-02 10:29:36 +0800

    Object操作

---
+++ index.js
@@ -0,0 +1,12 @@
+var user = {};
+user.name = "John";
+user.surname = "Mike";
+user.name = "Peter";
+delete user.name;
+
+var fruit = {
+	apple:20,
+	pear:20,
+	peach:10
+};
+var total = fruit.apple+fruit.pear+fruit.peach;
